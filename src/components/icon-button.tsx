fcfab20a090aa68a651ba09f40ea699c34d18150
Mônica Colomera <colomeramonica@gmail.com>
2025-02-18 13:06:41 -0300

    Fim Aula 01

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,11 +1,12 @@
-import { ComponentProps } from "react"
+import type { ComponentProps } from 'react'
 
 interface IconButtonProps extends ComponentProps<'button'> {}
 
-export function IconButton(props : IconButtonProps) {
-    return (
-        <button className="bg-gray-500 cursor-pointer duration-300 hover:bg-blue hover:text-gray-900 p-1.5 rounded-md text-blue transition-colors"
-        {...props}
-        />
-    )
+export function IconButton(props: IconButtonProps) {
+  return (
+    <button
+      className="bg-gray-500 cursor-pointer duration-300 hover:bg-blue hover:text-gray-900 p-1.5 rounded-md text-blue transition-colors"
+      {...props}
+    />
+  )
 }
--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,19 +1,34 @@
-import { Mail } from "lucide-react";
-import { ComponentProps } from "react";
+import { ComponentProps } from 'react';
 
-interface InputProps extends ComponentProps<'input'> {}
+interface InputRootProps extends ComponentProps<'div'> {
+  error?: boolean;
+}
 
-export function Input(props : InputProps) {
-    return (
-        <div className="bg-gray-800 border border-gray-600 flex focus-within:border-gray-100 gap-2 group h-12 items-center px-4 rounded-xl">
-            <span
-            className="group-[&:not(:has(input:placeholder-shown))]:text-gray-100 group-focus-within:text-gray-100 text-gray-400">
-            <Mail />
-            </span>
-        <input
-        className="flex-1 outline-none placeholder-gray-400"
-        {...props}
-        />
-        </div>
-    )
+export function InputRoot({ error = false, ...props }: InputRootProps) {
+  return (
+    <div
+      data-error={error}
+      className="bg-gray-800 border border-gray-600 data-[error=true]:border-danger flex focus-within:border-gray-100 gap-2 group h-12 items-center px-4 rounded-xl"
+      {...props}
+    />
+  );
+}
+
+interface InputIconProps extends ComponentProps<'span'> {}
+
+export function InputIcon(props: InputIconProps) {
+  return (
+    <span
+      className="group-[&:not(:has(input:placeholder-shown))]:text-gray-100 group-data-[error=true]:text-danger group-focus-within:text-gray-100 text-gray-400"
+      {...props}
+    />
+  );
+}
+
+interface InputFieldProps extends ComponentProps<'input'> {}
+
+export function InputField(props: InputFieldProps) {
+  return (
+    <input className="flex-1 outline-none placeholder-gray-400" {...props} />
+  );
 }
